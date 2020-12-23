6bacd4d085af41b579f02c256a581bd633826ef5
dodoto <279176202@qq.com>
2020-12-23 10:29:37 +0800

    promise reader folder constrctor tree

--- upload-utils.js
+++ upload-utils.js
@@ -1,38 +1,26 @@
-// import SparkMD5 from "spark-md5";
-// import aixos from 'axios';
-
-function computedMD5(file) {
-  // let time = new Date().getTime();
-  let md5 = "";
-  fileReader.readAsArrayBuffer(file);
-  fileReader.onload = e => {
-    if (file.size != e.target.result.byteLength) {
-      console.log("浏览器报成功但是一直不能读取文件信息");
-      return;
-    }
-    md5 = SparkMD5.ArrayBuffer.hash(e.target.result);
-    file.uniqueIdentifier = md5;
-  };
-}
-
-function isBigFile(file) {
-  // 2MB = 2 * 1024 * 1024
-  return file.size >= 2 * 1024 * 1024;
-}
 
 export function getReadFunc() {
   let timer = null;
   let files = [];
   let folders = [];
-  return function (items,callback) {
+  return function (items,callback,createFolder,upload) {
+    if(!callback) {
+      console.log('callback is required');
+      return
+    };
+    if(typeof callback !== 'function') {
+      console.log('callback is not a functioin');
+      return
+    };
     if(timer) clearTimeout(timer);
     timer = setTimeout(()=>{
       console.log('timer',timer);
       console.log('build',folders.length);
+      // createFolder()
       console.log('upload',files);
+      // upload()
       callback = null;
     },500);
-  
     items.forEach(item => {
       let target;
       if(item.webkitGetAsEntry) {
@@ -53,28 +41,61 @@ export function getReadFunc() {
   }
 }
 
-// function post() {
-//   return new Promise((resolve,reject) => {
-//     aixos.post(url,data)
-//     .then(res => {
-//       resolve(res.data);
-//     })
-//     .catch(err => {
-//       reject(err);
-//     })
-//   })
-// }
 
-// function get() {
-//   return new Promise((resolve, reject) => {
-//     axios.get(url, {
-//       params: params
-//     }).then(res => {
-//       resolve(res.data);
-//     }).catch(err => {
-//       reject(err);
-//     })
-//   })
-// }
+export function read(target) {
+  return new Promise((resolve,reject) => {
+    let index = 0;               //当前读取的目录下标
+    let qeue = [];               //目录数组
+    let uploadList = [];         //上传的文件  
+    let folders = [];            //创建的文件夹路径
+  
+    //归类
+    let sortOut = function(target) {
+      if(target.isFile) {
+        target.file(file=>{
+          uploadList.push({file,parentPath:target.fullPath});
+        });
+      }else{
+        folders.push(target.fullPath);
+        qeue.push(target);
+      };
+    }
+    
+    //读取目录
+    let readFolder = function(target) {
+      return new Promise((resolve,reject) => {
+        if(!target) {
+          reject('over')
+        }else{
+          let reader = target.createReader();
+          reader.readEntries(
+            entries => resolve(next(entries)),
+            error => reject(error)
+          );
+        };
+      });
+    }
+    
+    let next = function(entries) {
+      entries.forEach(entry => sortOut(entry));
+      index++;
+      let target = qeue[index];
+      return readFolder(target)
+    }
+  
+    sortOut(target);
+  
+    readFolder(qeue[index])
+    .then(res => next(res))
+    .catch(err => {
+      if(err === 'over') {
+        resolve({uploadList,folders});
+      }else{
+        reject('read failed');
+      }
+    })
+  })
+}
+
 
 
